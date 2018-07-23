f2b84ea4e60d350e2c5fab46a15ca1a177bb86a0
Anuj <anuj.me3@gmail.com>
2018-07-23 16:40:19 +0545

    remove unwanted code

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -41,69 +41,50 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
     }
 
-    public function authenticate(Request $request){
-
-        $request->validate([
-            'email' => 'required|email',
-            'password' => 'required',
-        ]);
-
-
-        $email = $request->input('email');
-        $password = $request->input('password');
-        if (Auth::attempt(['email' => $email, 'password' => $password,'type'=>'admin'])) {
-
-            // Authentication passed...
-            return redirect()->intended('/admin');
-        } else {
-
-            return redirect()->back()->with('error-message', 'Username or password didn\'t matched.');
-        }
-
-    }
     public function showLoginForm()
     {
         return view('auth.register');
     }
+
     public function login(Request $request)
     {
-        $validation=Validator::make($request->all(),[
+        $validation = Validator::make($request->all(), [
             'email' => 'required|email',
             'password' => 'required',
         ]);
 
-        if ($validation->fails())  {
+        if ($validation->fails()) {
             return response()->json($validation->errors()->toArray(), 422);
         }
 
         $email = $request->input('email');
         $password = $request->input('password');
 
-        $user_exist=User::where('email',$email)->first();
-        if(!$user_exist){
-            $data['email']='Account doesn\'t exist.';
+        $user_exist = User::where('email', $email)->first();
+        if (!$user_exist) {
+            $data['email'] = 'Account doesn\'t exist.';
             return response()->json($data, 422);
         }
 
-        $user = User::query()->where(['email'=>$email])->where('confirmed', 1)->first();
-        if(!$user){
-            $data['email']='Account not activated. Please check your email for the activation link.';
+        $user = User::query()->where(['email' => $email])->where('confirmed', 1)->first();
+        if (!$user) {
+            $data['email'] = 'Account not activated. Please check your email for the activation link.';
             return response()->json($data, 422);
         }
 
-        if (Auth::attempt(['email' => $email, 'password' => $password,'confirmed'=>1])) {
-                $data['success']=true;
-                $data['redirect']=$this->redirectTo;
-                return response()->json($data,200);
+        if (Auth::attempt(['email' => $email, 'password' => $password, 'confirmed' => 1])) {
+            $data['success'] = true;
+            $data['redirect'] = $this->redirectTo;
+            return response()->json($data, 200);
 
-        }else{
-            $data['email']='Credentials didn\'t match our records.';
+        } else {
+            $data['email'] = 'Credentials didn\'t match our records.';
             return response()->json($data, 422);
         }
     }
 
-    public function logout() {
-        // $user_type=Auth::user()->type;
+    public function logout()
+    {
         Auth::logout();
         return redirect('/');
     }
